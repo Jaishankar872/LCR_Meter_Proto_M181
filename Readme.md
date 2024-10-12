e33249c9b952c6c8b84dffbadf330dc3f5dbaf7e
Jaishankar872 <shankarjai872@gmail.com>
2024-10-13 02:25:28 +0530

    Addtion of Basic files set 1

---
+++ Readme.md
@@ -0,0 +1,2 @@
+## Goal of this Project
+To Develop a Firmware for M181 LCR from JyeTech. This give a understanding in DIY LCR Meter considering as a prototype. 
---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#define ADC PA0
+#define AFC PA1
+
+#define LED PA5
+#define GS PA6
+#define VI PA7
+
+#define BTN1_HOLD PB13
+#define BTN2_SP PB14
+#define BTN3_RCL PB15
+
+void setup() {
+
+}
+
+void loop() {
+  
+}
+
